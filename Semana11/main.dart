2eeb938d681d0448b75750a85de8a5288920b752
EliLima18 <63461960+ElieneRodriguesLima@users.noreply.github.com>
2021-06-12 20:58:15 -0300

    Create main.dart

---
+++ Semana11/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Navegation Basics",
+    home: FirstRoute(),
+    theme: ThemeData(
+    primarySwatch: Colors.pink,
+    ),
+  ));
+}
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Primeira Pagina'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Abrir segunda rota'),
+          onPressed: () {
+            Navigator.push(context,MaterialPageRoute(builder: (context) => SecondRoute()),);
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Eliene Rodrigues LIma"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Retornar!'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
