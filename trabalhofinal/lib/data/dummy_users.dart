21f436c1a39456f8e3c29069c15ceb9bfc7f0203
EliLima18 <63461960+ElieneRodriguesLima@users.noreply.github.com>
2021-07-08 20:42:45 -0300

    Create dummy_users.dart

---
+++ trabalhofinal/lib/data/dummy_users.dart
@@ -0,0 +1,32 @@
+import 'package:trabalhofinal/models/usuarios.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@alunos.com.br',
+    matricula: '2020111mtds0425',
+    avatarUrl: 'https://pixabay.com/it/vectors/donna-rosso-femmina-persona-995164/avatar-woman-995164_1280.png'
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Rafael',
+    email: 'rafael@alunos.com.br',
+    matricula: '2020111mtds1902',
+    avatarUrl: 'https://pixabay.com/it/vectors/femminile-donna-anonimo-utente-296989/avatar-female-296990_640.png'
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Fernanda',
+    email: 'fernanda@alunos.com.br',
+    matricula: '2020111mtds0506',
+    avatarUrl: 'https://pixabay.com/it/illustrations/pinguino-carino-beanie-5487301/avatar-penguin-5487301_640.png'
+  ),
+  '4': const User(
+    id: '4',
+    name: 'Guilherme',
+    email: 'guilherme@alunos.com.br',
+    matricula: '2020111mtds0609',
+    avatarUrl: 'https://pixabay.com/it/illustrations/donna-sorriso-elegante-bellezza-6275290/avatar-woman-6275290_640.png'
+  ),
+};
